9ef7c21ba0d32e8028b61063d76f8bd0fbb47c73
Skryvvara <darkyne.gaming@outlook.de>
2023-01-18 20:26:33 +0100

    fix: removed unused import

--- src/tests/useLocalStorage.test.tsx
+++ src/tests/useLocalStorage.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { renderToString } from 'react-dom/server';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { useLocalStorage } from '..';
