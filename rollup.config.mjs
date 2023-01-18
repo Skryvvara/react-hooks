8958433eba70e83805c478efb21d6799a6d37470
Skryvvara <darkyne.gaming@outlook.de>
2023-01-18 17:37:23 +0100

    feat: added terser and peer-deps-external plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import packageJson from './package.json' assert { type: "json" };
 
 export default [
@@ -24,9 +26,11 @@ export default [
       }
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" })
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser()
     ]
   },
   {
